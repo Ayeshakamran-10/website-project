68b65e919ea64228cf56ba2fe2a6825fae7aec72
Ayesha and Azal <8eayeshakamran@gmail.com>
2026-09-11 23:02:36 +0500

    modified files

--- script.js
+++ script.js
@@ -1,43 +1,28 @@
 function showMenu() {
-    document.getElementById("menu").scrollIntoView({
-        behavior: "smooth"
-    });
+    document.getElementById("menu").scrollIntoView({behavior: "smooth"});
 }
-
 
 function orderFood(foodName) {
     document.getElementById("message").value = foodName;
-
-    document.getElementById("contact").scrollIntoView({
-        behavior: "smooth"
-    });
+    document.getElementById("contact").scrollIntoView({behavior: "smooth"});
 }
 
+function toggleMenu() {
+    document.getElementById("navMenu").classList.toggle("show");
+}
+
+function closeMenu() {
+    document.getElementById("navMenu").classList.remove("show");
+}
 
 document.getElementById("contactForm").addEventListener("submit", function(event) {
-
     event.preventDefault();
-
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
-    var order = document.getElementById("order").value;
+    var order = document.getElementById("message").value;
     var address = document.getElementById("address").value;
-
     var phoneNumber = "923002899964";
-
-    var message =
-        "Hello DOM Restaurant!" +
-        "\n\nName: " + name +
-        "\nEmail: " + email +
-        "\nOrder: " + order +
-        "\nAddress: " + address;
-
-    var whatsappLink =
-        "https://wa.me/" +
-        phoneNumber +
-        "?text=" +
-        encodeURIComponent(message);
-
+    var message = "Hello DOM Restaurant!" + "\n\nName: " + name + "\nEmail: " + email + "\nOrder: " + order + "\nAddress: " + address;
+    var whatsappLink = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
     window.location.href = whatsappLink;
-
 });
